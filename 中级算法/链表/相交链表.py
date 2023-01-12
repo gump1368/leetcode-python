a97b6a559af507b5c2ebcdd2a25a442f32e70b3d
guan_dongpu <guan_dongpu@gowild.cn>
2023-01-12 15:02:30 +0800

    链表

--- 中级算法/链表/相交链表.py
+++ 中级算法/链表/相交链表.py
@@ -16,7 +16,7 @@ class Solution(object):
         :rtype: ListNode
         """
         p1, p2 = headA, headB
-        if p1 != p2:
+        while p1 != p2:
             p1 = p1.next if p1 else headB
             p2 = p2.next if p2 else headA
         return p1
